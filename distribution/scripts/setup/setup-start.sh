255e7983fa62d68781224582e98a4604416b926b
daksithj <daksithj@gmail.com>
2020-07-24 17:24:11 +0530

    Added script to install docker

--- distribution/scripts/setup/setup-start.sh
+++ distribution/scripts/setup/setup-start.sh
@@ -12,16 +12,21 @@ export script_directory="$repo_directory/distribution/scripts"
 
 # Start by cloning the performance test repository
 git clone https://daksithj:85351a39913458d12edb15f8f9ad0ed576d8251e@github.com/ldclakmal/ballerina-performance-aws-ecs.git $repo_directory
-if [ -d $repo_directory ]; then
+if [ ! -d $repo_directory ]; then
   echo "Could not pull the ecs performance test repository."
   exit 1
 fi
-if [ -d $script_directory ]; then
+if [ ! -d $script_directory ]; then
   echo "Script directory not available"
   exit 1
 fi
 
+# Install docker
+chmod +x $script_directory/docker/install-docker.sh
+$script_directory/docker/install-docker.sh
+
 
 # Install Java
+chmod +x $script_directory/java/install-java.sh
 $script_directory/java/install-java.sh
 
