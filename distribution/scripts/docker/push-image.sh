c8423162f358f640754271f908319f1d4ef9b09e
daksithj <daksithj@gmail.com>
2020-07-28 09:27:11 +0530

    Added script to make ballerina images

--- distribution/scripts/docker/push-image.sh
+++ distribution/scripts/docker/push-image.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Copyright 2017 WSO2 Inc. (http://wso2.org)
+#!/bin/bash
+# Copyright 2020 WSO2 Inc. (http://wso2.org)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
