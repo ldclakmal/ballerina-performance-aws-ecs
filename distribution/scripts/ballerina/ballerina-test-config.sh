7a324c762098981ac9704e4330ebde08b30a70d5
SmithAbey <shamithnirmal@gmail.com>
2021-03-17 13:20:22 +0530

    Add test scenarios

--- distribution/scripts/ballerina/ballerina-test-config.sh
+++ distribution/scripts/ballerina/ballerina-test-config.sh
@@ -29,9 +29,35 @@ declare -A test_scenario0=(
     [jmx]="http-post-request.jmx"
     [protocol]="http"
     [port]="9090"
+    [netty_options]=""
     [skip]=false
 )
-
+declare -A test_scenario1=(
+    [name]="h1c_transformation"
+    [display_name]="JSON to XML transformation HTTP service"
+    [description]="An HTTP Service, which transforms JSON requests to XML and then forwards all requests to an HTTP back-end service."
+    [bal]="h1c_transformation.jar"
+    [bal_flags]=""
+    [path]="/transform"
+    [jmx]="http-post-request.jmx"
+    [protocol]="http"
+    [port]="9090"
+    [netty_options]=""
+    [skip]=false
+)
+# declare -A test_scenario2=(
+#     [name]="h1_h1_passthrough"
+#     [display_name]="Passthrough HTTPS service (h1 -> h1)"
+#     [description]="An HTTPS Service, which forwards all requests to an HTTPS back-end service."
+#     [bal]="h1_h1_passthrough.jar"
+#     [bal_flags]=""
+#     [path]="/passthrough"
+#     [jmx]="http-post-request.jmx"
+#     [protocol]="https"
+#     [port]="9090"
+#     [netty_options]="-s true "
+#     [skip]=false
+# )
 function before_execute_test_scenario() {
     local bal_file=${scenario[bal]}
     local bal_flags=${scenario[bal_flags]}
@@ -39,6 +65,6 @@ function before_execute_test_scenario() {
     local protocol=${scenario[protocol]}
     local port=${scenario[port]}
     jmeter_params+=("host=$hostname" "port=$port" "path=$service_path")
-    jmeter_params+=("payload=./${msize}B.json" "response_size=${msize}B" "protocol=$protocol")
+    jmeter_params+=("payload=$GITHUB_REPO_DIR/components/payload-generator/target/${msize}B.json" "response_size=${msize}B" "protocol=$protocol")
     JMETER_JVM_ARGS="-Xbootclasspath/p:/opt/alpnboot/alpnboot.jar"
 }
